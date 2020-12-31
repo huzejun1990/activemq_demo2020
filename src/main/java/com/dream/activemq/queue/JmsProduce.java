f5aee0d6729ef4d9ed281427c6e801f13d386fd4
huzejun <961639130@qq.com>
2020-12-31 22:10:19 +0800

    初次提交 activemq代码 2020年12月31日22:10:9

---
+++ src/main/java/com/dream/activemq/queue/JmsProduce.java
@@ -0,0 +1,37 @@
+package com.dream.activemq.queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * @Author: huzejun
+ * @Date: 2020/12/31 21:09
+ */
+public class JmsProduce {
+
+    public static final String ACTIVEMQ_URL = "tcp://192.168.31.60:61616";
+    public static final String QUEUE_NAME = "queue01";
+
+    public static void main(String[] args) throws JMSException {
+        //1.创建连接工场,按照给定的url地址，采用默认用户名和密码
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+        //2.通过连接工场，获得连接connection并启动访问
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+
+        //3.创建会话session
+        //两个参数，第一叫事务，第二叫签收
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        //4.创建目的地（具体是队列还是主题topic）
+        Queue queue = session.createQueue(QUEUE_NAME);
+        //5.创建消息的生产者
+        MessageProducer messageProducer = session.createProducer(queue);
+        //6 通过使用消息生产者生产3条消息发送到MQ的队列里面
+        for (int i = 1; i <= 3; i++) {
+            //7 创建消息
+        }
+
+
+    }
+}
