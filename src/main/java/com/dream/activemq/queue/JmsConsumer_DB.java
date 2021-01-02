d7856d54d80266dc1c0e162743e5ed5d22017097
huzejun <961639130@qq.com>
2021-01-03 01:16:07 +0800

    spring整合activemq到jdbc最优版

--- src/main/java/com/dream/activemq/queue/JmsConsumer_DB.java
+++ src/main/java/com/dream/activemq/queue/JmsConsumer_DB.java
@@ -1,6 +1,5 @@
 package com.dream.activemq.queue;
 
-import lombok.SneakyThrows;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
@@ -11,89 +10,29 @@ import java.io.IOException;
  * @Date: 2021/1/3 0:27
  */
 public class JmsConsumer_DB {
-  /*  private static final String ACTIVEMQ_URL = "tcp://192.168.31.60:61616";
-    private static final String ACTIVEMQ_QUEUE_NAME = "Queue-JdbcPersistence";
+    private static final String ACTIVEMQ_URL = "tcp://192.168.31.60:61616";
+    private static final String QUEUE_NAME = "jdbc02";
 
     public static void main(String[] args) throws JMSException, IOException {
-        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
-        activeMQConnectionFactory.setBrokerURL(ACTIVEMQ_URL);
-        Connection connection = activeMQConnectionFactory.createConnection();
-        Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
-        Queue queue = session.createQueue(ACTIVEMQ_QUEUE_NAME);
-        MessageConsumer messageConsumer = session.createConsumer(queue);
-        connection.start();
-        messageConsumer.setMessageListener(new MessageListener() {
-            @SneakyThrows
-            @Override
-            public void onMessage(Message message) {
-                if (message instanceof TextMessage) {
-                    TextMessage textMessage = (TextMessage) message;
-                    session.commit();
-                    System.out.println("消费者收到消息" + textMessage.getText());
-                }
-            }
-        });
-        System.in.read();
-    }
-}*/
 
-    public static final String ACTIVEMQ_URL = "tcp://192.168.31.60:61616";
-    // public static final String ACTIVEMQ_URL = "nio://192.168.17.3:61608";
-    public static final String QUEUE_NAME = "jdbc 01 ";
-
-    public static void main(String[] args) throws Exception{
-        System.out.println(" 这里是 3 号 消费者 ");
-
-        // 1 按照给定的url创建连接工程，这个构造器采用默认的用户名密码
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
-        // 2 通过连接工厂连接 connection  和 启动
-        javax.jms.Connection connection = activeMQConnectionFactory.createConnection();
-        //  启动
+        Connection connection = activeMQConnectionFactory.createConnection();
         connection.start();
-        // 3 创建回话  session
-        // 两个参数，第一个事务， 第二个签收
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        // 4 创建目的地 （两种 ： 队列/主题   这里用队列）
-        Queue queue = session.createQueue(QUEUE_NAME);
-        // 5 创建消息的消费者
-        MessageConsumer messageConsumer = session.createConsumer(queue);
 
-      /*
-          同步阻塞方式reveive()   空参数的receive方法是阻塞，有参数的为等待时间
-          订阅者或消费者使用MessageConsumer 的receive() 方法接收消息，receive 在接收之前一直阻塞
-            while(true){
-            // 这里是 TextMessage 是因为消息发送者是 TextMessage ， 接受处理的
-            // 也应该是这个类型的消息
-            TextMessage message = (TextMessage)messageConsumer.receive(4000L);  // 4秒
-            if (null != message){
-                System.out.println("****消费者的消息："+message.getText());
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Queue queue = session.createQueue(QUEUE_NAME);
+        MessageConsumer messageConsumer = session.createConsumer(queue);
+        while (true){
+            TextMessage textMessage = (TextMessage) messageConsumer.receive(4000L);
+            if (null != textMessage) {
+                System.out.println("******消费者接收到消息： "+textMessage.getText());
             }else {
                 break;
             }
         }
-       */
-
-        // 通过监听的方式来消费消息
-        // 通过异步非阻塞的方式消费消息
-        // 通过messageConsumer 的setMessageListener 注册一个监听器，
-        // 当有消息发送来时，系统自动调用MessageListener 的 onMessage 方法处理消息
-        messageConsumer.setMessageListener(new MessageListener() {
-            public void onMessage(Message message)  {
-                if (null != message  && message instanceof TextMessage){
-                    TextMessage textMessage = (TextMessage)message;
-                    try {
-                        System.out.println("****消费者的消息："+textMessage.getText());
-                    }catch (JMSException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-
-        // 保证控制台不灭  不然activemq 还没连上就关掉了连接
-        System.in.read();
         messageConsumer.close();
         session.close();
         connection.close();
+
     }
 }
--- src/main/java/com/dream/activemq/queue/JmsProduceDB.java
+++ src/main/java/com/dream/activemq/queue/JmsProduceDB.java
@@ -3,81 +3,38 @@ package com.dream.activemq.queue;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
+import java.io.IOException;
 
 /**
  * @Author: huzejun
  * @Date: 2021/1/3 0:27
  */
 public class JmsProduceDB {
+    private static final String ACTIVEMQ_URL = "tcp://192.168.31.60:61616";
+    private static final String QUEUE_NAME = "jdbc02";
 
-   /* private static final String ACTIVEMQ_URL = "tcp://192.168.31.60:61616";
-    private static final String ACTIVEMQ_QUEUE_NAME = "Queue-JdbcPersistence";
+    public static void main(String[] args) throws JMSException, IOException {
 
-    public static void main(String[] args) throws JMSException
-    {
-        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
-        activeMQConnectionFactory.setBrokerURL(ACTIVEMQ_URL);
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
         Connection connection = activeMQConnectionFactory.createConnection();
-        Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
-        Queue queue = session.createQueue(ACTIVEMQ_QUEUE_NAME);
-        MessageProducer messageProducer = session.createProducer(queue);
-        messageProducer.setDeliveryMode(DeliveryMode.PERSISTENT);
         connection.start();
-        for (int i = 0; i < 3; i++) {
-            TextMessage textMessage = session.createTextMessage("Queue-JdbcPersistence测试消息" + i);
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Queue queue = session.createQueue(QUEUE_NAME);
+//        MessageConsumer messageConsumer = session.createConsumer(queue);
+        MessageProducer messageProducer = session.createProducer(queue);
+        for (int i = 1; i <= 3; i++) {
+            TextMessage textMessage = session.createTextMessage("jdbc msg---: " + i);
             messageProducer.send(textMessage);
+
         }
-        session.commit();
+//        session.commit();
         System.out.println("消息发送完成");
         messageProducer.close();
         session.close();
         connection.close();
-    }
-}
-*/
-
-    //  linux 上部署的activemq 的 IP 地址 + activemq 的端口号，如果用自己的需要改动
-    public static final String ACTIVEMQ_URL = "tcp://192.168.31.60:61616";
-    // public static final String ACTIVEMQ_URL = "nio://192.168.17.3:61608";
-    public static final String QUEUE_NAME = "jdbc01";
 
 
-    public static void main(String[] args) throws  Exception{
-
-        // 1 按照给定的url创建连接工程，这个构造器采用默认的用户名密码
-        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
-
-        // 设置允许有数据丢失
-        // activeMQConnectionFactory.setUseAsyncSend(true);
-
-        // 2 通过连接工厂连接 connection  和 启动
-        javax.jms.Connection connection = activeMQConnectionFactory.createConnection();
-        //  启动
-        connection.start();
-        // 3 创建回话  session
-        // 两个参数，第一个事务， 第二个签收
-        Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
-        // 4 创建目的地 （两种 ： 队列/主题   这里用队列）
-        Queue queue = session.createQueue(QUEUE_NAME);
-        // 5 创建消息的生产者
-        MessageProducer messageProducer = session.createProducer(queue);
-        // 非持久化消息 和持久化消息演示
-        messageProducer.setDeliveryMode(DeliveryMode.PERSISTENT);   // 持久化  如果开启
-        //       就会存入文件或数据库中
-
-        // 6 通过messageProducer 生产 3 条 消息发送到消息队列中
-        for (int i = 1; i < 4 ; i++) {
-            // 7  创建字消息
-            TextMessage textMessage = session.createTextMessage("msg--" + i);
-            // 8  通过messageProducer发布消息
-            messageProducer.send(textMessage);
-        }
-        // 9 关闭资源
-        messageProducer.close();
-        session.close();
-        connection.close();
-        // session.commit();
-        System.out.println("  **** 消息发送到MQ完成 ****");
     }
 }
 
